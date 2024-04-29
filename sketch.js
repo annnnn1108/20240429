4f858a18395109d5e09031418d0322fdf69a0e1e
annnnn1108 <150638646+annnnn1108@users.noreply.github.com>
2024-04-29 11:24:00 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,6 @@ function setup() {
   createCanvas(windowWidth,windowHeight);
   capture = createCapture(VIDEO) //啟動攝影機
   capture.size(capture_width,capture_height);//設定顯示畫面大小
-  captureGraphics =createGraphics(capture_width,capture_height)
   captureGraphics =createGraphics(capture_width,480)
   captureGraphics.translate(capture_width,0)
   captureGraphics.scale(-1,1)
@@ -16,6 +15,7 @@ function setup() {
   radioElement.position(width/2-300,20)
   radioElement.option("方塊")
   radioElement.option("圓圈")
+  radioElement.option("亮度")
   radioElement.style("color","#fff")
   radioElement.style("font-size","30")
 }
@@ -28,15 +28,20 @@ function draw() {
   var span=10
   push()
   translate(width/2-capture_width, height/2-capture_height) //把原點移到(width/2-160, height/2-120)
-  image(capture,0,0) //在(0,0)
+  captureGraphics.image(capture,0,0) //在(0,0)
   for(var x =0;x<captureGraphics.width;x=x+span){
     for(var y=0;y<captureGraphics.height ;y=y+span){
       var pixel =captureGraphics.get(x,y)
       fill(pixel)
-      if(radioElement.value()=="'方塊"){
+      if(radioElement.value()=="方塊"){
         rect(x,y,span)
       }
-      if(radioElement.value()=="'圓圈"){
+      if(radioElement.value()=="圓圈"){
+        ellipse(x,y,span)
+      }
+      if(radioElement.value()=="亮度"){
+        bk= (pixel[0]+pixel[1]+pixel[2])/3
+        fill(bk)
         ellipse(x,y,span)
       }
     }
